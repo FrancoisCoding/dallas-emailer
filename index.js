b5b546f6996e04f84c650b58f4534a4e8503cb49
Isaiah <francoisisaiaht@yahoo.com>
2021-07-12 18:25:07 -0400

    Initial repository

--- index.js
+++ index.js
@@ -24,21 +24,19 @@ var transporter = nodemailer.createTransport({
   },
 });
 
-console.log();
-
 app.listen(port, () => console.log("listening on port " + port));
 app.post("/", (req, res) => {
   req.body.entity
     ? (mailOptions = {
         from: process.env.USER,
-        to: "info@quadruplejcapital.com",
-        subject: "Quadruple J Capital Contact",
-        text: `Hi, my name is ${req.body.first_name} ${req.body.last_name}. Please reach out to me with additional information. My email address is ${req.body.email} and my phone number is ${req.body.phone}. My entity is ${req.body.entity}.`,
+        to: "francoisisaiaht@yahoo.com",
+        subject: "True Inspections Contact",
+        text: `Hi, my name is ${req.body.first_name} ${req.body.last_name}. Please reach out to me with additional information. My email address is ${req.body.email} and my phone number is ${req.body.phone}. Type of inspection is ${req.body.entity}.`,
       })
     : (mailOptions = {
         from: process.env.USER,
-        to: "info@quadruplejcapital.com",
-        subject: "Quadruple J Capital Contact",
+        to: "francoisisaiaht@yahoo.com",
+        subject: "True Inspections Contact",
         text: `Hi, my name is ${req.body.first_name} ${req.body.last_name}. Please reach out to me with additional information. My email address is ${req.body.email} and my phone number is ${req.body.phone}.`,
       });
   transporter.sendMail(mailOptions, function (error, info) {
